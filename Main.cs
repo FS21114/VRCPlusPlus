7f231e25749daded978019c4d22524b97bf6e9f5
FS21114 <58641402+FS21114@users.noreply.github.com>
2022-11-05 19:05:54 +0100

    Add files via upload

--- Main.cs
+++ Main.cs
@@ -6,26 +6,12 @@ namespace VRCPlusPlus
 	class Main : MelonMod
 	{
 		string username;
+		bool reset;
 		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
 		{
 			if (sceneName != "loader" && sceneName != "Application2" && sceneName != "ui")
 			{
-				if (sceneName == "Main")
-				{
-					username = "LoliGoddess";
-				}
-				else if (sceneName == "PrisonEscape")
-				{
-					username = "Ostinyo";
-				}
-				else if (sceneName == "Skeld")
-				{
-					username = "Jar";
-				}
-				else
-				{
-					username = null;
-				}
+				reset = true;
 			}
 			else
 			{
@@ -34,6 +20,11 @@ namespace VRCPlusPlus
 		}
 		public override void OnLateUpdate()
 		{
+			if (reset && RoomManager.field_Internal_Static_ApiWorld_0.authorName != null)
+			{
+				reset = false;
+				username = RoomManager.field_Internal_Static_ApiWorld_0.authorName;
+			}
 			if (username != null)
 			{
 				Networking.LocalPlayer.displayName = username;
