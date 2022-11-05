cf964149e22c1b8a97b810b19d0163fd7b249e5f
FS21114 <58641402+FS21114@users.noreply.github.com>
2022-11-05 07:39:40 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using MelonLoader;
+using VRC.SDKBase;
+
+namespace VRCPlusPlus
+{
+	class Main : MelonMod
+	{
+		string username;
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			if (sceneName != "loader" && sceneName != "Application2" && sceneName != "ui")
+			{
+				if (sceneName == "Main")
+				{
+					username = "LoliGoddess";
+				}
+				else if (sceneName == "PrisonEscape")
+				{
+					username = "Ostinyo";
+				}
+				else if (sceneName == "Skeld")
+				{
+					username = "Jar";
+				}
+				else
+				{
+					username = null;
+				}
+			}
+			else
+			{
+				username = null;
+			}
+		}
+		public override void OnLateUpdate()
+		{
+			if (username != null)
+			{
+				Networking.LocalPlayer.displayName = username;
+			}
+		}
+	}
+}
